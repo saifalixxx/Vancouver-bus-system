13d0e6bb5959e99d8d69c3948ede0172f5a161c4
saifalixxx <72938723+saifalixxx@users.noreply.github.com>
2022-04-01 12:54:07 +0100

    Add files via upload
    Need to fix a lot of errors, possibly add a protected class

--- TST.java
+++ TST.java
@@ -45,11 +45,51 @@ public class TST
                     }
                         station[count] = newScan.next();
                 }
+
+                String titleSt = station[2];
+                String title = titleSt.substring(0,2);
+
+                // Move keywords flagstop, wb, nb, sb, eb from start of the names to the end of the names of the stops
+                if(title.equals("NB") || title.equals("WB") || title.equals("SB") || title.equals("EB"))
+                {
+                    titleSt = titleSt.substring(3).concat(" " + title);
+                }
+
+                stData data = new stData(station);
+                connect(titleSt.toCharArray(), data);
+                newScan.close();
+            }
+            scan.close();
+        }
+        catch (FileNotFoundException error)
+        {
+            error.printStackTrace();
+        }
+    }
+
+    private void connect(char [] stID, stData data)
+    {
+        if(stID.length != 0)
+        {
+            if(branch == null)
+            {
+                branch = new Node(stID[0], -1);
+                ifEnded = false;
+                connect(stID, 0, branch, data);
             }
         }
-        catch (Exception error)
-        {
+    }
 
+    private Node connect(char[] stID, int count, Node newNode, stData data)
+    {
+        if(newNode == null)
+        {
+            newNode = new Node(stID[count], -1);
         }
+        if(newNode.ans < stID[count])
+        {
+            newNode.l = connect(stID, count, newNode.l, data);
+        }
+        else if()
     }
 }
