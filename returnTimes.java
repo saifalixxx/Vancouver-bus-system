01a2d709daca03dbe28ed24f9644c6ba401713b1
saifalixxx <72938723+saifalixxx@users.noreply.github.com>
2022-04-03 17:19:24 +0100

    Add files via upload
    I have completed the third part of the question paper. I have tried making sure to count for errors and exception. I will now set up my main class and work on the costs associated with edges. I will now be taking a break and start working back on the project around 6pm.

--- returnTimes.java
+++ returnTimes.java
@@ -8,6 +8,46 @@ import java.io.*;
 
 public class returnTimes
 {
+    // Sorting algorithm which will sort out the different times and get them ordered in the right way
+    // to allow the results to be printed in a much easier way.
+    public static ArrayList<String> sortingAlgorithm(ArrayList<String> inventory)
+    {
+        if (inventory.size() > 1)
+        {
+            ArrayList<String> few = new ArrayList<String>();
+            ArrayList<String> more = new ArrayList<String>();
+            ArrayList<String> ordered = new ArrayList<String>();
+
+            String key = inventory.get(inventory.size() - 1);
+
+            int count = 0;
+            while (count < inventory.size() - 1)
+            {
+                if (inventory.get(count).compareTo(key) >= 0)
+                {
+                    more.add(inventory.get(count));
+                }
+                else
+                {
+                    few.add(inventory.get(count));
+                }
+                count++;
+            }
+            few = sortingAlgorithm(few);
+            more = sortingAlgorithm(more);
+            few.add(key);
+            few.addAll(more);
+            ordered = few;
+
+            return ordered;
+        }
+        else
+        {
+            return inventory;
+        }
+
+    }
+
     public static ArrayList<String> readingData()
     {
         ArrayList<String> readingData = new ArrayList<String>();
@@ -73,6 +113,34 @@ public class returnTimes
             }
         });
         return routes;
+    }
+
+    // This function gets and prints out the results that the user desires to find, this class will be called
+    // in the main function to allow it to print out the results
+    public static boolean fetchOutput(String output)
+    {
+        ArrayList<String> inventory = readingData();
+        ArrayList<String> print = searchRoutes(output, inventory);
+
+        print = sortingAlgorithm(print);
+
+        if (print.isEmpty())
+        {
+            System.out.println("There exist no routes for which you have requested");
+            return false;
+        }
+        else
+        {
+            System.out.println(print.size() + "Here are the existing routes with the same times ↓ ");
+
+            int count = 0;
+            while (count < print.size())
+            {
+                System.out.println(print.get(count));
+                count++;
+            }
+        }
+        return true;
     }
 
 }
