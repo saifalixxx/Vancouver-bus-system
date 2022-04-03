db9d7cd33e1642456f07851069d06503fb75c772
saifalixxx <72938723+saifalixxx@users.noreply.github.com>
2022-04-04 00:49:07 +0100

    Add files via upload

--- Edge.java
+++ Edge.java
@@ -1,3 +1,5 @@
+// Sets up direct edges and vertices to allow the class to tack Q1 and also Q2 on the paper
+
 import java.util.Iterator;
 
 public class Edge<Object> implements Iterable<Object>
@@ -8,6 +10,23 @@ public class Edge<Object> implements Iterable<Object>
     {
         Object Object;
         Node thereAfter;
+    }
+
+    private class triverseInven implements Iterator<Object>
+    {
+        private Node present = newNode;
+
+        public boolean hasNext()
+        {
+            return present != null;
+        }
+
+        public Object next()
+        {
+            Object Object = present.Object;
+            present =  present.thereAfter;
+            return Object;
+        }
     }
 
     void add(Object Object)
@@ -21,3 +40,68 @@ public class Edge<Object> implements Iterable<Object>
     public Iterator<Object> iterator()
     {
         return new triverseInven();
+
+    }
+}
+
+// This class sets up vertices, starting from no edges at all.
+class costOfGrid
+{
+    private static final String NEXT_PAR = System.getProperty("line.separator");
+    private final int TEMP_VAR;
+    private int VAR;
+    private Edge<costOfEdge>[] diE;
+
+    public costOfGrid(int TEMP_VAR)
+    {
+        this.TEMP_VAR = TEMP_VAR;
+        this.VAR = 0;
+        diE = (Edge<costOfEdge>[]) new Edge[TEMP_VAR];
+
+        int count = 0;
+        while (count < TEMP_VAR) {
+            diE[count] = new Edge<costOfEdge>();
+            count++;
+        }
+    }
+
+    public void connect(costOfEdge cost)
+    {
+        int var = cost.from();
+        diE[var].add(cost);
+        VAR++;
+
+    }
+
+    public Iterable<costOfEdge> diE(int var)
+    {
+        return diE[var];
+    }
+
+    public int TEMP_VAR()
+    {
+        return TEMP_VAR;
+    }
+
+    public int VAR()
+    {
+        return VAR;
+    }
+
+}
+
+// Allows me to paste immutable data-only classes.
+record costOfEdge(int var, int tempVar, double cost)
+{
+    public int from()
+    {
+        return var;
+    }
+
+    public int to()
+    {
+        return tempVar;
+    }
+}
+
+
